542aaddfdb9b50ae144aa48d02a889a05c505961
BesuBaru <mp645slix@gmail.com>
2019-05-28 19:35:12 -0400

    Clean Up Solution and Added Update Fork
    Cleaned up the solution, so it's easier to browse to find the correct source or header files.
    
    Also added is an update fork batch, for easy updating without having to delete the fork every time a pull request happens.

---
+++ Update.bat
@@ -0,0 +1,6 @@
+git remote add upstream git://github.com/lybxlpsv/TotallyLegitArcadeController.git
+git fetch upstream
+git pull upstream experimental
+
+cd %LocalAppData%\GitHubDesktop
+GitHubDesktop.exe
